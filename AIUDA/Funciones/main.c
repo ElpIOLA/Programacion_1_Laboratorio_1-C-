51a5f270f043a4549247f5a2dbf719e145528620
Elpiola <porve.cabj@gmail.com>
2018-04-19 10:09:21 -0300

    Agregamos las clases

--- AIUDA/Funciones/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include "FunCpIOLA.h"
-
-int main()
-{
-    return 0;
-}
---
+++ Clase_03/Ejercicio 2/misFunciones.h
@@ -0,0 +1,8 @@
+
+int suma(int n1, int n2);
+
+float dividirEnteros(int n1,int n2);
+
+int pedirNumeroPar();
+
+int pedirNumero(char* mensaje, char* errorMensaje, int maximo, int minimo);
