91138f5b3b35e2d1f5828f4035b801346c036ea5
Elpiola <porve.cabj@gmail.com>
2018-04-19 10:10:37 -0300

    Agregamos Tarea

--- Clase_06/TareaClase6/main.c
+++ Clase_06/TareaClase6/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define CANT 20
 
 int main()
 {
-    char nombre[30], apellido[30], nombreYapellido[30];
+    char nombre[CANT], apellido[CANT], nombreYapellido[CANT];
 
     printf("\n\tIngrese Nombre: ");
     gets(nombre);
     printf("\n\tIngrese Apellido: ");
     gets(apellido);
 
-    if(strcmp(nombre,apellido)!=0){
+    if(stricmp(nombre,apellido)!=0){
         strcpy(nombreYapellido,strcat(nombre,apellido)); /**STRCAT: CONCATENA DIFERENTES VECTORES*/
         printf("\n\tNombre y Apellido: [ %s ]\n",nombreYapellido);
     }else{
